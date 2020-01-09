750d025ff6f267f74f282f50709b86785d7ac6bf
Joe Olley <joeolley@hotmail.co.uk>
2020-01-09 16:02:41 +0000

    app routes and general css updated

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react';
-import { Router, Route , Link} from "react-router-dom";
+import { Router, Route } from "react-router-dom";
 import history from './history';
 
 import Dashboard from './components/dashboard'
 import Login from './components/login'
 import Register from './components/register'
 import News from './components/dashboard_pages/news'
+import Sport from './components/dashboard_pages/sport'
+import Photo from './components/dashboard_pages/photos'
+import Task from './components/dashboard_pages/tasks'
 
 import './App.css';
 
@@ -19,6 +22,9 @@ class App extends Component {
         <Route path="/dashboard" component={Dashboard} />
         <Route path="/register" component={Register} />
         <Route path="/news" component={News} />
+        <Route path="/sport" component={Sport} />
+        <Route path="/photos" component={Photo} />
+        <Route path="/tasks" component={Task} />
       </div>
     </Router>
     )
